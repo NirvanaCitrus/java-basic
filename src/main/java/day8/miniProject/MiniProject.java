ce0b59eda72c689820ba73a773a669d75149c2b9
NirvanaCitrus <whiteclark123@gmail.com>
2024-09-03 15:52:53 +0900

    req.6 DetailFunctionComplete

--- src/main/java/day8/miniProject/MiniProject.java
+++ src/main/java/day8/miniProject/MiniProject.java
@@ -1,5 +1,7 @@
 package day8.miniProject;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,38 +12,54 @@ public class MiniProject {
 
     }
 }
+
 class Post {
     private int id;
     private String title;
     private String context;
+    private LocalDate creationDate;
+    private LocalTime creationTime;
 
-    public Post (int id, String title, String context) {
+    public Post(int id, String title, String context, LocalDate creationDate, LocalTime creationTime) {
         this.id = id;
         this.title = title;
         this.context = context;
+        this.creationDate = creationDate;
+        this.creationTime = creationTime;
 
     }
-    public int getId () {
+
+    public int getId() {
         return id;
     }
-    public String getTitle () {
+
+    public String getTitle() {
         return title;
     }
-    public String getContext () {
+
+    public String getContext() {
         return context;
     }
-    public void setTitle (String title) {
+
+    public void setTitle(String title) {
         this.title = title;
     }
-    public void setContext (String context) {
+
+    public void setContext(String context) {
         this.context = context;
+    }
+    public LocalDate getCreationDate () {
+        return creationDate;
+    }
+    public LocalTime getCreationTime () {
+        return creationTime;
     }
 }
 
 class Homepage {
     Scanner sc = new Scanner(System.in);
-    ArrayList<String> titles = new ArrayList<>();
-    ArrayList<String> contexts = new ArrayList<>();
+    ArrayList<Post> posts = new ArrayList<>();
+    int postIdCounter = 1; // 가장 최신의 id값. id값의 고유성을 유지하기 위해 1씩 증가시킬 계획임.
 
     public void Start() {
         while (true) {
@@ -54,53 +72,82 @@ class Homepage {
                 break;
 
             } else if (command.equals("add")) {
-                System.out.println("게시물 제목을 입력해주세요 : ");
+                System.out.println("게시물 제목을 입력해주세요: ");
                 String title = sc.nextLine();
-                titles.add(title);
-                System.out.println("게시물 내용을 입력해주세요 : ");
-                String context = sc.nextLine();
-                contexts.add(context);
-                System.out.println("게시물이 등록되었습니다.");
+                System.out.println("게시물 내용을 입력해주세요: ");
+                String content = sc.nextLine();
+
+                Post newPost = new Post(postIdCounter, title, content, LocalDate.now(), LocalTime.now());
+                posts.add(newPost);
+
+                System.out.println(postIdCounter + "번 게시물이 등록되었습니다.");
+                postIdCounter++;
 
             } else if (command.equals("list")) {
-                for (int i = 0; i < titles.size(); i++) {
-                    System.out.println("게시물 번호: " + (i + 1));
-                    System.out.println("제목 : " + titles.get(i));
-                    System.out.println("내용 : " + contexts.get(i));
+                for (Post post : posts) {
+                    System.out.println("게시물 번호: " + post.getId());
+                    System.out.println("제목: " + post.getTitle());
+                    System.out.println("-----");
 
                 }
 
             } else if (command.equals("update")) {
                 System.out.println("수정할 게시물 번호를 입력하세요");
-                int index = Integer.parseInt(sc.nextLine()) - 1;
+                int id = Integer.parseInt(sc.nextLine());
+                boolean found = false;
 
-                if (index >= 0 && index < titles.size()) {
-                    System.out.println("기존 제목: " + titles.get(index));
-                    System.out.println("새로운 제목을 입력하세요: ");
-                    String newTitle = sc.nextLine();
-                    titles.set(index, newTitle);
+                for (Post post : posts) {
 
-                    System.out.println("기존 내용: " + contexts.get(index));
-                    System.out.println("새로운 내용을 입력하세요: ");
-                    String newContext = sc.nextLine();
-                    contexts.set(index, newContext);
+                    if (post.getId() == id) {
+                        System.out.println("기존 제목: " + post.getTitle());
+                        System.out.println("새로운 제목을 입력하세요: ");
+                        String newTitle = sc.nextLine();
+                        post.setTitle(newTitle);
 
-                    System.out.println((index + 1) + "번 게시물이 수정되었습니다.");
+                        System.out.println("기존 내용: " + post.getContext());
+                        System.out.println("새로운 내용을 입력하세요: ");
+                        String newContext = sc.nextLine();
+                        post.setContext(newContext);
 
-                } else {
-                    System.out.println("없는 게시물 번호입니다.");
-                    continue;
+                        System.out.println(id + "번 게시물이 수정되었습니다.");
+                        found = true;
+                        break;
+                    }
+                    if (!found) {
+                        System.out.println("없는 게시물 번호입니다.");
+
+                    }
                 }
-
-
             } else if (command.equals("delete")) {
                 System.out.println("삭제할 게시물 번호를 입력하세요: ");
-                int index = Integer.parseInt(sc.nextLine()) - 1;
+                int id = Integer.parseInt(sc.nextLine());
+                boolean found = false;
 
-                if (index >= 0 && index < titles.size()) {
-                    titles.remove(index);
-                    contexts.remove(index);
-                    System.out.println((index + 1) + "번 게시물이 삭제되었습니다.");
+                for (Post post : posts) {
+                    if (post.getId() == id) {
+                        posts.remove(post);
+                        System.out.println(id + "번 게시물이 삭제되었습니다.");
+                        found = true;
+                        break;
+                    }
+                }
+            } else if (command.equals("detail")) {
+                System.out.println("상세보기 할 게시물 번호를 입력해주세요 : ");
+                int id = Integer.parseInt(sc.nextLine());
+                boolean found = false;
+
+                for (Post post : posts) {
+                    if (post.getId() == id) {
+                        System.out.println("게시물 번호 : " + post.getId());
+                        System.out.println("제목 : " + post.getTitle());
+                        System.out.println("내용 : " + post.getContext());
+                        System.out.println("작성일 : " + post.getCreationDate() + "/ " + post.getCreationTime());
+                        System.out.println("-----");
+                        found = true;
+                        break;
+
+
+                    }
                 }
 
             }
